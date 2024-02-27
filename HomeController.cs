fd4904bd5a39404030a191c2e267a15427a068d5
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2024-02-27 15:22:09 +0700

    Create HomeController.cs
    tạo action Index1

---
+++ HomeController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using xuatthongtin.Models;
+
+namespace xuatthongtin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            var empl = new Employee();
+            ViewData["empl"] = empl;
+            return View();
+        }
+	}
+}
